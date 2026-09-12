869d34f340c6f8fd7effe77ead00a9557ec3268c
lwalton101 <waltonfamily01@outlook.com>
2026-09-12 11:37:19 +0100

    fix stutter

--- ui/Window.py
+++ ui/Window.py
@@ -1,4 +1,5 @@
-from pyray import VIOLET,WHITE, begin_drawing, Color, clear_background, close_window, draw_fps, draw_text, end_drawing, get_frame_time, init_window, is_key_pressed, poll_input_events, set_target_fps, set_window_title, window_should_close
+from pyray import VIOLET,WHITE, begin_drawing, Color, clear_background, close_window, draw_fps, draw_text, end_drawing, get_frame_time, init_window, is_key_pressed, poll_input_events, set_config_flags, set_target_fps, set_window_title, window_should_close
+from raylib import FLAG_MSAA_4X_HINT, FLAG_VSYNC_HINT, FLAG_WINDOW_UNDECORATED
 
 from ui.WindowState import WindowState
 from ui.widget.PolyWidget import PolyWidget
@@ -11,11 +12,26 @@ class Window:
     state: WindowState
     def __init__(self):
         self.state = WindowState()
+        set_config_flags(FLAG_MSAA_4X_HINT | FLAG_VSYNC_HINT | FLAG_WINDOW_UNDECORATED)
         init_window(1280, 800, self.state.window_title.get())
-        set_target_fps(self.state.fps_cap.get())
+        #set_target_fps(self.state.fps_cap.get())
 
         self.state.window_title.subscribe(self.on_title_changed)
         self.state.fps_cap.subscribe(self.on_fps_cap_changed)
+
+        self.root_widget = Widget(self.state, 0,0)
+        self.rect_widget = RectWidget(self.state, 0, 0, 500, 30, Color(255,0,0,255))
+        self.rect_widget_two = RectWidget(self.state, 0, 50, 500, 30, Color(0,0,255,255))
+        self.rect_widget_three = RectWidget(self.state, 0, 50, 500, 30, Color(0,255,0,255))
+
+        self.triangle = PolyWidget(self.state, 500, 600, 15, 150, 45.5, Color(0, 255, 0, 255))
+        self.root_widget.children.append(self.rect_widget)
+        self.root_widget.children.append(self.triangle)
+        self.rect_widget.children.append(self.rect_widget_two)
+        self.rect_widget_two.children.append(self.rect_widget_three)
+
+        self.rotating_rect = PolyWidget(self.state, 500, 500, 4, 150, 150, VIOLET)
+        self.root_widget.children.append(self.rotating_rect)
         pass
 
     def on_title_changed(self, title: str):
@@ -27,23 +43,16 @@ class Window:
     def render(self) -> bool:
         begin_drawing()
         clear_background(WHITE)  # noqa: F821
-        root_widget = Widget(self.state, 0,0)
-        rect_widget = RectWidget(self.state, self.startX, self.startY, 500, 30, Color(255,0,0,255))
-        rect_widget_two = RectWidget(self.state, 0, 50, 500, 30, Color(0,0,255,255))
-        rect_widget_three = RectWidget(self.state, 0, 50, 500, 30, Color(0,255,0,255))
 
-        triangle = PolyWidget(self.state, 500, 600, 6, 150, 0, Color(0, 255, 0, 255))
-        root_widget.children.append(rect_widget)
-        root_widget.children.append(triangle)
-        rect_widget.children.append(rect_widget_two)
-        rect_widget_two.children.append(rect_widget_three)
 
-        self.startX += 10 * get_frame_time()
+        self.rect_widget.x += 100 * get_frame_time()
+        self.rotating_rect.rotation += 100 * get_frame_time()
 
-        if self.startX > 1280:
-            self.startX = 0
 
-        root_widget.render(0,0)
+        if self.rect_widget.x > 1280:
+            self.rect_widget.x = 0
+
+        self.root_widget.render(0,0)
         draw_fps(10,10)
         end_drawing()
         return window_should_close()
--- ui/WindowState.py
+++ ui/WindowState.py
@@ -3,4 +3,4 @@ from util.TrackedVar import TrackedVar
 
 class WindowState:
     window_title: TrackedVar[str] = TrackedVar("Sound Box")
-    fps_cap: TrackedVar[int] = TrackedVar(60)
+    fps_cap: TrackedVar[int] = TrackedVar(0)
